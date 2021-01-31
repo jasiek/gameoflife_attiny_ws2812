a83966f2590ec8ca638edb269d6a2aa4a5b08a30
Jan Szumiec <jan.szumiec@gmail.com>
2021-01-31 18:17:27 +0100

    More tests passing.

--- test/test_board.cpp
+++ test/test_board.cpp
@@ -14,11 +14,21 @@ void test_init(void) {
       TEST_ASSERT_EQUAL(1, g.board.getLit(x, y));
 }
 
+void test_finished(void) {
+  GameOfLife g;
+  unsigned long long one = 1;
+  g.init(&one);
+  TEST_ASSERT_EQUAL(0, g.isFinished());
+
+  g.board.clearBoard();
+  TEST_ASSERT_EQUAL(1, g.isFinished());
+}
+
 void test_count_neighbours(void) {
   GameOfLife g;
-  unsigned long long all_lit = 18446744073709551615LL;
+  unsigned long long state = 18446744073709551615LL;
 
-  g.init(&all_lit);
+  g.init(&state);
   for (byte x = 0; x < BOARD_SIZE; x++)
     for (byte y = 0; y < BOARD_SIZE; y++)
       TEST_ASSERT_EQUAL(8, g.countNeighbours(x, y));
@@ -27,6 +37,12 @@ void test_count_neighbours(void) {
   for (byte x = 0; x < BOARD_SIZE; x++)
     for (byte y = 0; y < BOARD_SIZE; y++)
       TEST_ASSERT_EQUAL(0, g.countNeighbours(x, y));
+
+  state = 1;
+  g.init(&state);
+  TEST_ASSERT_EQUAL(0, g.countNeighbours(0, 0));
+  TEST_ASSERT_EQUAL(1, g.countNeighbours(1, 1));
+		    
 }
 
 // board.cpp
@@ -70,6 +86,7 @@ void setup() {
   RUN_TEST(test_decay);
   RUN_TEST(test_init);
   RUN_TEST(test_count_neighbours);
+  RUN_TEST(test_finished);
 }
 
 void loop() {
